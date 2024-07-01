1221f9325306a12e7c94889845f3acda2e492108
bossearch <yudha.bosse@gmail.com>
2024-07-02 00:50:15 +0800

    adding my keymaps

--- keyboards/crkbd/rev1/keymaps/bosse/config.h
+++ keyboards/crkbd/rev1/keymaps/bosse/config.h
@@ -6,10 +6,8 @@
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
-#define OLED_FONT_H "keyboards/crkbd/rev1/keymaps/bosse/glcdfont.c"
+#define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
 #define DYNAMIC_KEYMAP_LAYER_COUNT 5
 #define MOUSEKEY_INTERVAL 6
 #define MOUSEKEY_MAX_SPEED 5
 #define MOUSEKEY_MOVE_DELTA 4
-#define HOLD_ON_OTHER_KEY_PRESS
-#define ONESHOT_TAP_TOGGLE 2
--- keyboards/crkbd/rev1/keymaps/bosse/keymap.c
+++ keyboards/crkbd/rev1/keymaps/bosse/keymap.c
@@ -1,68 +1,60 @@
 #include QMK_KEYBOARD_H
 
-#include "layers.h"
-#include "oled.c"
-#include "macros.c" //Refer to macros-example.c if you want to use macro. If you don't go to line 60 ish.
+ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
-[_QWERTY] = LAYOUT_split_3x6_3(
-    LT(_NAV,KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_GRV,
+ [0] = LAYOUT_split_3x6_3(
+    KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, QK_GESC,
     KC_MEH, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
     KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ENT,
-    KC_LGUI, KC_ESC, LT(_SYM,KC_SPC), LT(_NUM,KC_BSPC), OSM(MOD_LSFT), KC_LALT
+    KC_LSFT, KC_LGUI, LT(1,KC_SPC), LT(2,KC_BSPC), KC_LALT, TG(4)
     ),
 
-[_SYM] = LAYOUT_split_3x6_3(
-    KC_TAB, KC_EXLM, KC_AT, KC_HASH, KC_LPRN, KC_CIRC, KC_BSLS, KC_RPRN, KC_PMNS, KC_PPLS, KC_EQL, KC_GRV,
-    KC_MEH, KC_AMPR, KC_DLR, KC_PERC, KC_LBRC, KC_UNDS, KC_PIPE, KC_RBRC, KC_PSLS, KC_PAST, KC_COLN, KC_DQUO,
-    KC_LCTL, MCR0, MCR1, MCR2, KC_LCBR, KC_LT, KC_GT, KC_RCBR, KC_COMM, KC_DOT, KC_QUES, KC_ENT,
-    KC_LGUI, KC_ESC, KC_TRNS, LT(_NUM,KC_BSPC), KC_LSFT, KC_LALT
+[1] = LAYOUT_split_3x6_3(
+    KC_TAB, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_GRV,
+    KC_MEH, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PMNS, KC_PSLS, KC_PPLS, KC_LBRC, KC_RBRC, KC_BSLS,
+    KC_LCTL, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_UNDS, KC_PIPE, KC_PEQL, KC_LCBR, KC_RCBR, KC_ENT,
+    KC_LSFT, KC_LGUI, KC_TRNS, LT(2,KC_BSPC), KC_LALT, KC_NO
     ),
 
-[_NUM] = LAYOUT_split_3x6_3(
-    KC_TAB, KC_1, KC_2, KC_3, KC_4, KC_5, KMCR0, KMCR1, KMCR2, KMCR3, KMCR4, KC_GRV,
-    KC_MEH, KC_0, KC_9, KC_8, KC_7, KC_6, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_MSTP, KC_MUTE,
-    KC_LCTL, KC_UNDO, KC_CUT, KC_COPY, KC_PSTE, KC_WBAK, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_MPLY, KC_ENT,
-    KC_LGUI, KC_ESC, LT(_SYM,KC_SPC), KC_TRNS, KC_LSFT, KC_LALT
+[2] = LAYOUT_split_3x6_3(
+    KC_TAB, KC_P1, KC_P2, KC_P3, KC_P4, KC_P5, KC_P6, KC_P7, KC_P8, KC_P9, KC_P0, KC_GRV,
+    KC_MEH, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_NO,
+    KC_LCTL, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_ENT,
+    KC_LSFT, KC_LGUI, LT(1,KC_SPC), KC_TRNS, KC_LALT, KC_NO
     ),
 
-[_NAV] = LAYOUT_split_3x6_3(
-    KC_TRNS, QK_BOOT, KC_F1, KC_F2, KC_F3, KC_F4, KC_WH_L, KC_WH_U, KC_WH_D, KC_WH_R, KC_PSCR, TG(_GAME),
-    GU_ON, CG_LSWP, KC_F5, KC_F6, KC_F7, KC_F8, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_INS, KC_HOME,
-    GU_OFF, CG_LNRM, KC_F9, KC_F10, KC_F11, KC_F12, MCR3, KC_PGDN, KC_PGUP, MCR4, KC_END, KC_DEL,
-    KC_LGUI, KC_LSFT, KC_LALT, KC_BTN1, KC_BTN2, KC_BTN3
+[3] = LAYOUT_split_3x6_3(
+    QK_BOOT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P7, KC_P8, KC_P9, KC_P0, KC_GRV,
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P4, KC_P5, KC_P6, KC_NO, KC_NO,
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P1, KC_P2, KC_P3, KC_NO, KC_ENT,
+    KC_LSFT, KC_LGUI, KC_TRNS, KC_TRNS, KC_LALT, KC_NO
     ),
 
-[_GAME] = LAYOUT_split_3x6_3(
-    KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_GRV,
-    KC_MEH, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
-    KC_RCTL, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ENT,
-    KC_LALT, KC_LSFT, KC_SPC, LT(_NUM,KC_BSPC), LT(_SYM,KC_ESC), KC_RGUI
-    ),
+[4] = LAYOUT_split_3x6_3(
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_VOLU, KC_VOLU, KC_MPLY, KC_MRWD, KC_MFFD, KC_NO,
+    KC_NO, KC_NO, KC_NO, KC_BTN1, KC_BTN2, KC_NO, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_NO, KC_NO,
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_WH_U, KC_WH_D, KC_WH_U, KC_WH_R, KC_NO, KC_NO,
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS
+    )
 
 };
 
-// layer_state_t layer_state_set_user(layer_state_t state) {
-// return update_tri_layer_state(state, 1, 2, 3);
-// };
+layer_state_t layer_state_set_user(layer_state_t state) {
+return update_tri_layer_state(state, 1, 2, 3);
+};
 
 // Shift + Backspace = Delete
-const key_override_t delete_ctrl_override = ko_make_basic(MOD_MASK_CTRL, LT(_NUM,KC_BSPC), KC_DEL);
-const key_override_t delete_gui_override = ko_make_basic(MOD_MASK_GUI, LT(_NUM,KC_BSPC), KC_DEL);
+const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, LT(2,KC_BSPC), KC_DEL);
+
+// Shift + esc = ~
+const key_override_t tilde_esc_override = ko_make_basic(MOD_MASK_SHIFT, KC_ESC, S(KC_GRV));
+
+// GUI + esc = `
+const key_override_t grave_esc_override = ko_make_basic(MOD_MASK_GUI, KC_ESC, KC_GRV);
+
 const key_override_t **key_overrides = (const key_override_t *[]){
-	&delete_ctrl_override,
-	&delete_gui_override,
-    NULL
+	&tilde_esc_override,
+	&grave_esc_override,
+    &delete_key_override,
+	NULL
 };
-
-#include "combos.c"
-
-//!!! Remove these comment below before compiling without using macro.
-// bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-//     if (record->event.pressed) {
-//         oled_timer = timer_read32();
-//         add_keylog(keycode);
-//     }
-//     return true;
-// };
