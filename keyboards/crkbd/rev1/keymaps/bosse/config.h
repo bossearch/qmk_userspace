108e82cccb53efefe1950caebc40a623bbb852c3
bossearch <yudha.bosse@gmail.com>
2024-07-14 17:10:46 +0800

    little adjustment

--- keyboards/crkbd/rev1/keymaps/bosse/config.h
+++ keyboards/crkbd/rev1/keymaps/bosse/config.h
@@ -6,7 +6,7 @@
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
-#define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
+#define OLED_FONT_H "keyboards/crkbd/rev1/keymaps/bosse/glcdfont.c"
 #define DYNAMIC_KEYMAP_LAYER_COUNT 5
 #define MOUSEKEY_INTERVAL 6
 #define MOUSEKEY_MAX_SPEED 5
---
+++ keyboards/crkbd/rev1/keymaps/bosse/layers.h
@@ -0,0 +1,8 @@
+#ifndef _LAYERS_DEFINED
+#define _LAYERS_DEFINED
+#define _QWERTY 0
+#define _SYM 1
+#define _NUM 2
+#define _NAV 3
+#define _GAME 4
+#endif
