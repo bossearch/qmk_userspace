a2ac1cdd47e569f7113a833383b2cc6594539e10
bossearch <yudha.bosse@gmail.com>
2024-07-21 22:48:42 +0800

    add new oled icons

--- keyboards/crkbd/rev1/keymaps/bosse/combos.c
+++ keyboards/crkbd/rev1/keymaps/bosse/combos.c
@@ -1,4 +1,4 @@
-const uint16_t PROGMEM spc_ent_combo[] = {KC_TAB, QK_GESC, COMBO_END};
+const uint16_t PROGMEM spc_ent_combo[] = {KC_TAB, KC_GRV, COMBO_END};
 
 // clang-format off
 combo_t key_combos[] = {
