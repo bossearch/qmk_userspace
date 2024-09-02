0ac80e375d63b3e37dedabc567fecbfa4b02f06f
bossearch <yudha.bosse@gmail.com>
2024-09-03 02:04:21 +0800

    adding new macros

---
+++ keyboards/crkbd/rev1/keymaps/bosse/config.h
@@ -0,0 +1,15 @@
+#pragma once
+
+/* Select hand configuration */
+
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
+
+#define OLED_FONT_H "keyboards/crkbd/rev1/keymaps/bosse/glcdfont.c"
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+#define MOUSEKEY_INTERVAL 6
+#define MOUSEKEY_MAX_SPEED 5
+#define MOUSEKEY_MOVE_DELTA 4
+#define HOLD_ON_OTHER_KEY_PRESS
+#define ONESHOT_TAP_TOGGLE 2
---
+++ keyboards/crkbd/rev1/keymaps/bosse/macros-example.c
@@ -0,0 +1,38 @@
+enum custom_keycodes {
+    QMKBEST = SAFE_RANGE,
+    QMKURL,
+    MY_OTHER_MACRO,
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+    case QMKBEST:
+        if (record->event.pressed) {
+            // when keycode QMKBEST is pressed
+            SEND_STRING("QMK is the best thing ever!");
+        } else {
+            // when keycode QMKBEST is released
+        }
+        break;
+
+    case QMKURL:
+        if (record->event.pressed) {
+            // when keycode QMKURL is pressed
+            SEND_STRING("https://qmk.fm/\n");
+        } else {
+            // when keycode QMKURL is released
+        }
+        break;
+
+    case MY_OTHER_MACRO:
+        if (record->event.pressed) {
+           SEND_STRING(SS_LCTL("ac")); // selects all and copies
+        }
+        break;
+    }
+    if (record->event.pressed) {
+        oled_timer = timer_read32();
+        add_keylog(keycode);
+    }
+    return true;
+};
