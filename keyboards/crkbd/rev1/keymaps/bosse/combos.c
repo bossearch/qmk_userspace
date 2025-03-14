611f0441d6a50e96f8357857f0574c65a11f84ff
bossearch <yudha.bosse@gmail.com>
2025-03-14 18:47:35 +0800

    new keymapping
    - add KC_HYPR and remove double mod key RCTL
    - modify combos to output certain key presses

--- keyboards/crkbd/rev1/keymaps/bosse/combos.c
+++ keyboards/crkbd/rev1/keymaps/bosse/combos.c
@@ -1,11 +1,42 @@
-const uint16_t PROGMEM toggle_qwerty_combo[] = {KC_TAB, KC_T, COMBO_END};
-const uint16_t PROGMEM toggle_ctrl_gui_combo[] = {KC_LCTL, KC_LGUI, COMBO_END};
-const uint16_t PROGMEM toggle_alt_gui_combo[] = {KC_LALT, KC_LGUI, COMBO_END};
+enum combos { GAME_COMBO, QWERTY_COMBO };
 
-// clang-format off
+const uint16_t PROGMEM game_combo_keys[]     = {KC_MEH, KC_G, COMBO_END};
+const uint16_t PROGMEM qwerty_combo_keys[] = {KC_ESC, KC_G, COMBO_END};
+const uint16_t PROGMEM swap_ctrl_gui[] = {KC_LCTL, KC_LGUI, COMBO_END};
+
 combo_t key_combos[] = {
-    COMBO(toggle_qwerty_combo, TO(_QWERTY)),
-    COMBO(toggle_ctrl_gui_combo, CG_TOGG),
-    COMBO(toggle_alt_gui_combo, AG_TOGG),
+    [GAME_COMBO]   = COMBO(game_combo_keys, TG(_GAME)),
+    [QWERTY_COMBO] = COMBO(qwerty_combo_keys, TO(_QWERTY)),
+    COMBO(swap_ctrl_gui, CG_TOGG),
 };
-// clang-format on
+
+// Action when the combo keys are released
+bool process_combo_key_release(uint16_t combo_index, combo_t *combo, uint8_t key_index, uint16_t keycode) {
+    switch (combo_index) {
+        case GAME_COMBO:
+            if (key_index == 0 || key_index == 1) {
+            /*if (keycode == KC_MEH || keycode == KC_G) {*/
+                register_code(KC_LCTL);
+                register_code(KC_LALT);
+                register_code(KC_LSFT);
+                tap_code(KC_G);
+                unregister_code(KC_LCTL);
+                unregister_code(KC_LALT);
+                unregister_code(KC_LSFT);
+            }
+            return true;
+        case QWERTY_COMBO:
+            if (key_index == 0 || key_index == 1) {
+            /*if (keycode == KC_ESC || keycode == KC_G) {*/
+                register_code(KC_LCTL);
+                register_code(KC_LALT);
+                register_code(KC_LSFT);
+                tap_code(KC_G);
+                unregister_code(KC_LCTL);
+                unregister_code(KC_LALT);
+                unregister_code(KC_LSFT);
+            }
+            return true;
+    }
+    return false;
+}
--- keyboards/crkbd/rev1/keymaps/bosse/config.h
+++ keyboards/crkbd/rev1/keymaps/bosse/config.h
@@ -8,6 +8,7 @@
 
 #define OLED_FONT_H "keyboards/crkbd/rev1/keymaps/bosse/glcdfont.c"
 #define DYNAMIC_KEYMAP_LAYER_COUNT 5
+#define COMBO_PROCESS_KEY_RELEASE
 
 #define MOUSEKEY_DELAY              10    // Delay between pressing a movement key and cursor movement
 #define MOUSEKEY_INTERVAL           6.061 // Time between cursor movements in milliseconds
