96cb6af7085b378a12c2051ddfc1bd77bf12d232
bossearch <yudha.bosse@gmail.com>
2025-04-26 21:06:28 +0800

    change keypad number to normal number
    - cause someapp don't always recognize keypad number
    - turn off auto numlock function

--- keyboards/crkbd/rev1/keymaps/bosse/keymap.c
+++ keyboards/crkbd/rev1/keymaps/bosse/keymap.c
@@ -28,8 +28,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
 [_NUM] = LAYOUT_split_3x6_3(
-    KC_TAB, KC_P1, KC_P2, KC_P3, KC_P4, KC_P5, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_MPLY, WWW,
-    KC_MEH, KC_P0, KC_P9, KC_P8, KC_P7, KC_P6, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_MUTE, KC_WFWD,
+    KC_TAB, KC_1, KC_2, KC_3, KC_4, KC_5, KC_MRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_MLY, WWW,
+    KC_MEH, KC_0, KC_9, KC_8, KC_7, KC_6, KC_LEFT, KC_DOWN, KC_U, KC_RGHT, KC_MUTE, KC_WFWD,
     KC_HYPR, KC_UNDO, KC_CUT, KC_COPY, KC_PSTE, CADEL, AF4, KC_BRID, KC_BRIU, TTY1, TTY2, KC_WBAK,
     KC_LCTL, ALT_ESC, OSM_LALT, KC_TRNS, KC_LSFT, KC_LGUI
     ),
@@ -56,12 +56,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // };
 
 // NUMLOCK AUTO ON
-bool led_update_user(led_t led_state) {
-	if (!led_state.num_lock) {
-		tap_code(KC_NUM_LOCK);
-	}
-	return true;
-}
+// bool led_update_user(led_t led_state) {
+// 	if (!led_state.num_lock) {
+// 		tap_code(KC_NUM_LOCK);
+// 	}
+// 	return true;
+// }
 
 // Shift + Backspace = Delete
 //const key_override_t delete_ctrl_override = ko_make_basic(MOD_MASK_CTRL, LT(_NUM,KC_BSPC), KC_DEL);
