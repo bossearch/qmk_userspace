10ddfcbad94262e4a61700a7ab5356badae1dc9b
bossearch <yudha.bosse@gmail.com>
2025-08-08 22:57:19 +0800

    fix: change other keypad key to normal key

--- keyboards/crkbd/rev1/keymaps/bosse/combos.c
+++ keyboards/crkbd/rev1/keymaps/bosse/combos.c
@@ -1,8 +1,8 @@
 enum combos { GAME_COMBO, QWERTY_COMBO };
 
-const uint16_t PROGMEM game_combo_keys[]     = {KC_MEH, KC_G, COMBO_END};
+const uint16_t PROGMEM game_combo_keys[]   = {KC_MEH, KC_G, COMBO_END};
 const uint16_t PROGMEM qwerty_combo_keys[] = {KC_ESC, KC_G, COMBO_END};
-const uint16_t PROGMEM swap_ctrl_gui[] = {KC_LCTL, KC_LGUI, COMBO_END};
+const uint16_t PROGMEM swap_ctrl_gui[]     = {KC_LCTL, KC_LGUI, COMBO_END};
 
 combo_t key_combos[] = {
     [GAME_COMBO]   = COMBO(game_combo_keys, TG(_GAME)),
@@ -15,7 +15,7 @@ bool process_combo_key_release(uint16_t combo_index, combo_t *combo, uint8_t key
     switch (combo_index) {
         case GAME_COMBO:
             if (key_index == 0 || key_index == 1) {
-            /*if (keycode == KC_MEH || keycode == KC_G) {*/
+                /*if (keycode == KC_MEH || keycode == KC_G) {*/
                 register_code(KC_LCTL);
                 register_code(KC_LALT);
                 register_code(KC_LSFT);
@@ -27,7 +27,7 @@ bool process_combo_key_release(uint16_t combo_index, combo_t *combo, uint8_t key
             return true;
         case QWERTY_COMBO:
             if (key_index == 0 || key_index == 1) {
-            /*if (keycode == KC_ESC || keycode == KC_G) {*/
+                /*if (keycode == KC_ESC || keycode == KC_G) {*/
                 register_code(KC_LCTL);
                 register_code(KC_LALT);
                 register_code(KC_LSFT);
