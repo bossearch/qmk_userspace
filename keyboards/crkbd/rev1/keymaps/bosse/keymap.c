867e98c84b731fd15378dfbe318558315414ea42
bossearch <yudha.bosse@gmail.com>
2025-09-15 18:25:42 +0800

    fix: change meh key on afk layout with grv

--- keyboards/crkbd/rev1/keymaps/bosse/keymap.c
+++ keyboards/crkbd/rev1/keymaps/bosse/keymap.c
@@ -42,9 +42,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
 [_GAME] = LAYOUT_split_3x6_3(
-    KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_GRV,
+    KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MEH,
     KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_MEH, KC_ENT,
+    KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ENT,
     KC_LCTL, KC_LALT, KC_SPC, NUM_BSPC, SYM_ESC, NAV_TAB
     ),
 
