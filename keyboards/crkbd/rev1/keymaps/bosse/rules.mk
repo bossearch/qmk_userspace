ed7349873f091e1012167467e3246113bb8c13ae
bossearch <yudha.bosse@gmail.com>
2026-01-09 20:37:45 +0800

    feat: add descriptions for each rules

--- keyboards/crkbd/rev1/keymaps/bosse/rules.mk
+++ keyboards/crkbd/rev1/keymaps/bosse/rules.mk
@@ -1,17 +1,40 @@
-MOUSEKEY_ENABLE     = yes
-VIA_ENABLE          = no
-LTO_ENABLE          = yes
+# Link Time Optimization
+LTO_ENABLE = yes
+# Emulate a mouse using keyboard
+MOUSEKEY_ENABLE = yes
+# Hit multiple keys at once and for different output
+COMBO_ENABLE = yes
+# OLED feature
+OLED_ENABLE = yes
+# Audio control and System control(+450)
+EXTRAKEY_ENABLE = yes
+# Nkey Rollover
+NKRO_ENABLE = yes
+# Override modifier key
 KEY_OVERRIDE_ENABLE = no
-COMBO_ENABLE        = yes
-EXTRAKEY_ENABLE     = yes # Audio control and System control(+450)
-CONSOLE_ENABLE      = no  # Console for debug(+400)
-COMMAND_ENABLE      = no  # Commands for debug and configuration
-NKRO_ENABLE         = no  # Nkey Rollover
-BACKLIGHT_ENABLE    = no  # Enable keyboard backlight functionality
-MIDI_ENABLE         = no  # MIDI controls
-AUDIO_ENABLE        = no  # Audio output on port C6
-UNICODE_ENABLE      = no  # Unicode
-BLUETOOTH_ENABLE    = no  # Enable Bluetooth with the Adafruit EZ-Key HID
-RGBLIGHT_ENABLE     = no  # Enable WS2812 RGB underlight.
-SWAP_HANDS_ENABLE   = no
-OLED_DRIVER_ENABLE  = yes
+# Support for one-handed typing without requiring a separate layer
+SWAP_HANDS_ENABLE = no
+# Enable configuring using VIA
+VIA_ENABLE = no
+# Game controller input
+JOYSTICK_ENABLE = no
+# Console for debug(+400)
+CONSOLE_ENABLE = no
+# Commands for debug and configuration
+COMMAND_ENABLE = no
+# Enable keyboard backlight functionality
+BACKLIGHT_ENABLE = no
+# MIDI controls
+MIDI_ENABLE = no
+# Audio output on port C6
+AUDIO_ENABLE = no
+# Unicode
+UNICODE_ENABLE = no
+# Enable Bluetooth with the Adafruit EZ-Key HID
+BLUETOOTH_ENABLE = no
+# Enable WS2812 RGB underlight.
+RGBLIGHT_ENABLE = no
+# Disabled on purpose to reduce firmware size
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no
+MAGIC_ENABLE = no
