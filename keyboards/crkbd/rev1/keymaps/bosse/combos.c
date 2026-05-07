da75328f0af80fb1f8b3d28737d2df0b93d55a87
bossearch <yudha.bosse@gmail.com>
2026-05-08 02:27:49 +0800

    fix: remove unused combo combinations

--- keyboards/crkbd/rev1/keymaps/bosse/combos.c
+++ keyboards/crkbd/rev1/keymaps/bosse/combos.c
@@ -1,34 +1,7 @@
 enum combos { GAME_COMBO, QWERTY_COMBO };
 
-const uint16_t PROGMEM game_combo_keys[]   = {KC_RALT, KC_G, COMBO_END};
-const uint16_t PROGMEM qwerty_combo_keys[] = {KC_ESC, KC_G, COMBO_END};
 const uint16_t PROGMEM swap_ctrl_gui[]     = {KC_LCTL, KC_LGUI, COMBO_END};
 
 combo_t key_combos[] = {
-    [GAME_COMBO]   = COMBO(game_combo_keys, TG(_GAME)),
-    [QWERTY_COMBO] = COMBO(qwerty_combo_keys, TO(_QWERTY)),
     COMBO(swap_ctrl_gui, CG_TOGG),
 };
-
-// Action when the combo keys are released
-bool process_combo_key_release(uint16_t combo_index, combo_t *combo, uint8_t key_index, uint16_t keycode) {
-    switch (combo_index) {
-        case GAME_COMBO:
-            if (key_index == 0 || key_index == 1) {
-                /*if (keycode == KC_RALT || keycode == KC_G) {*/
-                register_code(KC_RALT);
-                tap_code(KC_G);
-                unregister_code(KC_LALT);
-            }
-            return true;
-        case QWERTY_COMBO:
-            if (key_index == 0 || key_index == 1) {
-                /*if (keycode == KC_ESC || keycode == KC_G) {*/
-                register_code(KC_RALT);
-                tap_code(KC_G);
-                unregister_code(KC_RALT);
-            }
-            return true;
-    }
-    return false;
-}
